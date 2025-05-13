34ca7b41edfdd6f75803d8493bb866bc881eacc6
skyjiangnan <469830427@qq.com>
2025-05-13 10:17:49 +0800

    样式调整：修改到店套餐和超值代金券模块间距为16px

--- src/pages/SanqingtanRestaurant.jsx
+++ src/pages/SanqingtanRestaurant.jsx
@@ -151,7 +151,7 @@ const SanqingtanRestaurantDrawer = ({ onClose }) => {
 
       {/* 套餐和代金券 */}
       <div className="bg-white mt-0 px-3 py-3">
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-[16px]">
           {/* 到店套餐 */}
           <div className="bg-cyan-50 p-4 rounded-lg">
             <div className="flex justify-between items-center mb-4">
